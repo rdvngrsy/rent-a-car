24235a6c1a723a9d42f6d5220b2b8c62ea4c5ae9
Ridvan Gursoy <ridvangursoy@hotmail.com>
2023-11-20 15:55:36 +0300

    repository ve controller katmanları eklendi.

--- src/main/java/com/example/rentacar/controllers/BrandsController.java
+++ src/main/java/com/example/rentacar/controllers/BrandsController.java
@@ -1,9 +1,8 @@
 package com.example.rentacar.controllers;
 
 import com.example.rentacar.entities.Brand;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.rentacar.repositories.BrandRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -11,8 +10,39 @@ import java.util.List;
 @RequestMapping("api/brands")
 public class BrandsController {
 
+    private final BrandRepository brandRepository;
+
+    public BrandsController(BrandRepository brandRepository){
+        this.brandRepository = brandRepository;
+    }
+
+
     @GetMapping
     public List<Brand> getAll(){
-        return null;
+        return brandRepository.findAll(); // select * from brand gibi.
     }
+
+    @GetMapping("{id}")
+    public Brand getById(@PathVariable int id){
+        // Optional<T> => ilgili filtreden bir veri dönmeyebilir.Ondan orElseThrow gönderdik.
+        return brandRepository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public void add(@RequestBody Brand brand){
+        brandRepository.save(brand);
+    }
+
+    @PutMapping
+    public void update(@RequestBody Brand brand){}
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id) {
+        Brand brandToDelete = brandRepository.findById(id).orElseThrow();
+        brandRepository.delete(brandToDelete);
+
+        // yada aynı işlevi yapan bu. Yukarıdaki daha işlevsel kontrol yapabilir.
+        //brandRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/example/rentacar/entities/Brand.java
+++ src/main/java/com/example/rentacar/entities/Brand.java
@@ -1,11 +1,20 @@
 package com.example.rentacar.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Brand {
     @Id
     @Column(name = "id", unique = true)
@@ -16,6 +25,7 @@ public class Brand {
     private String name;
 
     @OneToMany(mappedBy = "brand")// Car classında brand attribute na mapleniyor.
+    @JsonIgnore                   // Bu alan jsona parse edilirken yok sayılcak. Sonsuz döngü var burda çünkü.
     private List<Car> cars;
 
     @OneToMany(mappedBy = "brand")// Model classında brand attribute na mapleniyor.
